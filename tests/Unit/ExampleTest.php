0feca0b314eab4a5c23f992bc72916c98ce0c044
MarcoAntonioOrnelas <MarcoAntonioOrnelas@users.noreply.github.com>
2023-11-19 22:04:07 -0600

    Commit con pruebas Fallidas

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,24 +1,20 @@
 <?php
-
 namespace Tests\Unit;
-
 use PHPUnit\Framework\TestCase;
-
 class ExampleTest extends TestCase
 {
-   
     public function testraiz():void{
-        $a=9;
+        $a=16;
         $raiz= app('App\Http\Controllers\ControladorTest')->root($a);
         $this->assertEquals(3, $raiz);
     }
     public function testEmpiezconA() : void {
         $pal ="Assert";
-        $caracter ="A";
+        $caracter ="B";
         $this->assertStringStartsWith($caracter,$pal);
     }
     public function testTienepalabra():void{
-        $frase = "Hola Gente";
+        $frase = "Buenas Tardes";
         $palabra = "Hola";
         $this->assertStringContainsString($palabra, $frase);
     }
@@ -26,11 +22,10 @@ class ExampleTest extends TestCase
         $multiplicando= 15;
         $multiplicador=4;
         $res = app('App\Http\Controllers\ControladorTest')-> Multi($multiplicando,$multiplicador);
-        $this->assertEquals(60,$res);
+        $this->assertEquals(35,$res);
     }
     public function testEsArreglo() : void {
-        $Arreglo = [];
+        $Arreglo = null;
         $this-> assertIsArray($Arreglo);
-    }
-    
+    }    
 }
