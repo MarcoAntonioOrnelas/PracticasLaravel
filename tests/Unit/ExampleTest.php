c1d66dd6be4e97c3d5cd75d264ae111b7c7e0150
ezio19r <ric-reyes@outlook.com>
2023-11-29 07:01:41 -0600

    test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,26 +6,26 @@ class ExampleTest extends TestCase
     public function testraiz():void{
         $a=16;
         $raiz= app('App\Http\Controllers\ControladorTest')->root($a);
-        $this->assertEquals(3, $raiz);
+        $this->assertEquals(4, $raiz);
     }
     public function testEmpiezconA() : void {
         $pal ="Assert";
-        $caracter ="B";
+        $caracter ="A";
         $this->assertStringStartsWith($caracter,$pal);
     }
     public function testTienepalabra():void{
         $frase = "Buenas Tardes";
-        $palabra = "Hola";
+        $palabra = "Buenas Tardes";
         $this->assertStringContainsString($palabra, $frase);
     }
     public function testMulti():void{
         $multiplicando= 15;
         $multiplicador=4;
         $res = app('App\Http\Controllers\ControladorTest')-> Multi($multiplicando,$multiplicador);
-        $this->assertEquals(35,$res);
+        $this->assertEquals(60,$res);
     }
     public function testEsArreglo() : void {
-        $Arreglo = null;
+        $Arreglo = [];
         $this-> assertIsArray($Arreglo);
     }    
 }
